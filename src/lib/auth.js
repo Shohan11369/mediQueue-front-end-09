6d7cb31267ef4adba333a0571e92ecb38d658b3e
Shohan111369 <sarkershohan346@gmail.com>
2026-05-23 15:00:02 +0800

    added auth client and auth js

---
+++ src/lib/auth.js
@@ -0,0 +1,34 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+const client = new MongoClient(process.env.MONGODB_URI);
+// console.log(process.env.MONGODB_URI);
+
+const db = client.db("mediQueue");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENTID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      allowLinking: true,
+    },
+  },
+
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: "jwt",
+      maxAge: 5 * 24 * 60 * 60, // in second
+    },
+  },
+  plugins: [jwt()],
+});
